77a04b41acf9ff02a4e8e0aa2733a99b312ba570
Andrew Walker <alwalker21@gmail.com>
2013-12-22 19:54:21 -0600

    messing around with logging.

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,7 +1,16 @@
 package com.alwalker.mllp_server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 class Server {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Logger logger = LoggerFactory.getLogger(Server.class);
+    	logger.info("Hello World");
+
+		// LoggerContext lc = (LoggerContext)LoggerFactory.getILoggerFactory();
+  //       StatusPrinter.print(lc);
 	}
 }
