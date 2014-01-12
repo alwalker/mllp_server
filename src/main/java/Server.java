0b57cfb477911ab5be8e51978061f1dae6a588f4
Andrew Walker <alwalker21@gmail.com>
2014-01-12 13:39:07 -0800

    Fixed casing error and removed FileIO

---
+++ src/main/java/Server.java
@@ -0,0 +1,12 @@
+package com.alwalker.mllp_server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+	static Logger logger = LoggerFactory.getLogger(Server.class);
+
+	public static void main(String[] args) {
+		logger.info("success!");
+	}
+}
